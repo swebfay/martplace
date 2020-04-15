79ba2d34debb2b57b19d5ac5a4ad8d5500c282ce
swebfay <swebfay@gmail.com>
2020-04-15 02:58:24 +0100

    HEADER-DASHBOARD-FEATURES

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ let gulp = require('gulp'),
 
 gulp.task('sass', function(){
     return gulp.src('app/scss/style.scss')
-            .pipe(sass({outputStyle: 'compressed'}))
+            .pipe(sass({outputStyle: 'expanded'}))
             .pipe(rename({suffix: '.min'}))
             .pipe(autoprefixer({
                 overrideBrowserslist: ['last 8 versions']
@@ -22,7 +22,8 @@ gulp.task('style', function() {
     return gulp.src([
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
+        'node_modules/rateyo/src/jquery.rateyo.css'
     ])
         .pipe(concat('libs.min.css'))
         .pipe(cssmin())
@@ -32,7 +33,9 @@ gulp.task('style', function() {
 gulp.task('script', function() {
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
+        'node_modules/mixitup/dist/mixitup.js',
+        'node_modules/rateyo/src/jquery.rateyo.js'
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
